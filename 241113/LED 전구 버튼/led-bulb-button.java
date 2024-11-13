7b6ab0d1bdb282ccf565aba987a1410af9a01c31
Soyou <loverduck97@naver.com>
2024-11-13 22:25:48 +0900

    Title: LED 전구 버튼, Time: 93ms, Memory: 9MB, Status: Runtime Error - CodeTree

---
+++ 241113/LED 전구 버튼/led-bulb-button.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder out = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int[] lights = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            lights[i] = Integer.parseInt(br.readLine());
+        }
+
+        for(int i = 0; i < b; i++) {
+            int[] newLights = new int[n];
+            newLights[0] = lights[n - 1] == 1 ? ( lights[0] == 1 ? 0 : 1) : lights[0];
+            for(int j = 1; j < n; j++) {
+                newLights[j] = lights[j - 1] == 1 ? ( lights[j] == 1 ? 0 : 1) : lights[j];
+            }
+            lights = newLights.clone();
+        }
+
+        for(int i = 0; i < n; i++) {
+            out.append(lights[i]).append("\n");
+        }
+
+        System.out.println(out);
+    }
+}
